c4c6922c9f65822a4c68056e53ec68cd00b4c37c
Corentin Frery <corentin.frery@gmail.com>
2018-04-14 12:25:34 -0400

    Simplification du calcul du bonheur

--- src/modele/Student.java
+++ src/modele/Student.java
@@ -62,37 +62,18 @@ public class Student {
 	
 	public int calculateHappiness() {
 		float happiness = 0;
-		happiness += calculateHappinessByGaming();
-		happiness += calculateHappinessByLove();
-		happiness += calculateHappinessBySchool();
-		happiness += calculateHappinessBySocial();
-		happiness += calculateHappinessByHealth();
-		happiness += calculateHappinessByRelaxation();
-		happiness += calculateHappinessByVitality();
+		happiness += calculateHappinessByPrimary(profile.getGaming(), state.getGaming());
+		happiness += calculateHappinessByPrimary(profile.getLove(), state.getLove());
+		happiness += calculateHappinessByPrimary(profile.getSchool(), state.getSchool());
+		happiness += calculateHappinessByPrimary(profile.getSocial(), state.getSocial());
+		happiness += calculateHappinessBySecondary(profile.getHealth(), state.getHealth());
+		happiness += calculateHappinessBySecondary(profile.getRelaxation(), state.getRelaxation());
+		happiness += calculateHappinessBySecondary(profile.getVitality(), state.getVitality());
 		return Math.round(happiness);
 	}
 	
-	public float calculateHappinessByGaming() {
-		float diff = state.getGaming()-profile.getGaming();
-		return calculateHappinessForPrimary(diff);
-	}
-	
-	public float calculateHappinessByLove() {
-		float diff = state.getLove()-profile.getLove();
-		return calculateHappinessForPrimary(diff);
-	}
-	
-	public float calculateHappinessBySchool() {
-		float diff = state.getSchool()-profile.getSchool();
-		return calculateHappinessForPrimary(diff);
-	}
-	
-	public float calculateHappinessBySocial() {
-		float diff = state.getSocial()-profile.getSocial();
-		return calculateHappinessForPrimary(diff);
-	}
-	
-	public float calculateHappinessForPrimary(float diff) {
+	private float calculateHappinessByPrimary(int profile, int state) {
+		float diff = state-profile;
 		if (diff >= 0) {
 			if (diff <= 20) 
 				return 8.75f+(20-diff)/20*8.75f;
@@ -107,21 +88,9 @@ public class Student {
 		}
 	}
 	
-	public float calculateHappinessByHealth() {
-		float num = state.getHealth()-profile.getHealth();
-		float denom = 100-profile.getHealth();
-		return (num/denom*10) > 0 ? num/denom*10 : 0;
-	}
-	
-	public float calculateHappinessByRelaxation() {
-		float num = state.getRelaxation()-profile.getRelaxation();
-		float denom = 100-profile.getRelaxation();
-		return (num/denom*10) > 0 ? num/denom*10 : 0;
-	}
-	
-	public float calculateHappinessByVitality() {
-		float num = state.getVitality()-profile.getVitality();
-		float denom = 100-profile.getVitality();
+	private float calculateHappinessBySecondary(int profile, int state) {
+		float num = state-profile;
+		float denom = 100-profile;
 		return (num/denom*10) > 0 ? num/denom*10 : 0;
 	}
 }
